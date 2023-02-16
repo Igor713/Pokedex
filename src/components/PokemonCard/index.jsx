3d8ef5fdf00483814ef29fc0bbeb15cc5a3dcf33
Jardelvorpagel <jardelvorpagel@gmail.com>
2023-02-15 22:16:20 -0300

    add async/await and Promise.all

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -1,30 +1,31 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 export default function PokemonCard({ name, url }) {
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={url}
-                    alt="green iguana"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {name}
-                    </Typography>
-                    {/* <Typography variant="body2" color="text.secondary">
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={url}
+          alt="green iguana"
+          sx={{ objectFit: "contain" }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          {/* <Typography variant="body2" color="text.secondary">
                         Lizards are a widespread group of squamate reptiles, with over 6,000
                         species, ranging across all continents except Antarctica
                     </Typography> */}
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    )
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,47 @@
-import { Container } from '@mui/system'
-import React, { useEffect, useState } from 'react'
-import PokemonCard from '../components/PokemonCard'
-import Navbar from '../components/Navbar'
-import { Grid } from '@mui/material'
-import axios from 'axios'
+import { Container } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import PokemonCard from "../components/PokemonCard";
+import Navbar from "../components/Navbar";
+import { Grid } from "@mui/material";
+import axios from "axios";
 
 export default function Home() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log(pokemons);
+  const getPokemons = async () => {
+    let endpoints = [];
 
-    const [pokemons, setPokemons] = useState([])
-
-    useEffect(() => {
-        getPokemons()
-    }, [])
-
-    const getPokemons = () => {
-        let endpoints = []
-
-        for (let i = 1; i < 50; i++) {
-            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-        }
-
-        let response = axios.all(endpoints.map((endpoint) => {
-            axios.get(endpoint)
-                .then((res) => setPokemons(res))
-                .catch((err) => console.log(err))
-        }))
-        console.log(response)
-        return response
+    for (let i = 1; i < 50; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     }
 
-    return (
-        <div>
-            <Navbar />
-            <Container maxWidth="false">
-                <Grid container spacing={2}>
-                    {pokemons.length === 0 && pokemons.map((pokemon, index) => (
-                        <Grid key={index} item xs={3}>
-                            <PokemonCard name={pokemon.name} image={pokemon.url} />
-                        </Grid>
-                    ))}
-                </Grid>
-            </Container>
-        </div>
-    )
+    const requests = endpoints.map((endpoint) => axios.get(endpoint));
+    const responses = Promise.all(requests);
+
+    const result = (await responses).map((response) => response.data);
+    setPokemons(result);
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Container maxWidth="false">
+        <Grid container spacing={2}>
+          {pokemons.length &&
+            pokemons.map((pokemon, index) => (
+              <Grid key={index} item xs={3}>
+                <PokemonCard
+                  name={pokemon.name}
+                  url={pokemon.sprites.other.dream_world.front_default}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      </Container>
+    </div>
+  );
 }
